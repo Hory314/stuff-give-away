5c189ad40e3a61ffd2591c041392cf7c82e09f31
Michał Hordyjewicz <hordyjewiczmichal@gmail.com>
2018-12-22 15:34:01 +0100

    initial

---
+++ src/main/java/pl/hordyjewicz/stuffgiveaway/StuffGiveAwayApplication.java
@@ -0,0 +1,16 @@
+package pl.hordyjewicz.stuffgiveaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuffGiveAwayApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(StuffGiveAwayApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/pl/hordyjewicz/stuffgiveaway/StuffGiveAwayApplicationTests.java
@@ -0,0 +1,19 @@
+package pl.hordyjewicz.stuffgiveaway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StuffGiveAwayApplicationTests
+{
+
+    @Test
+    public void contextLoads()
+    {
+    }
+
+}
+
